cbf847aa7ceb27599851568bf688787c80997ae8
dlermen12 <dennerl@hotmail.com>
2015-11-25 22:34:05 -0200

    Criação do Controller e confguração do Unity para injetar dependencias

---
+++ Controllers/PostController.cs
@@ -0,0 +1,39 @@
+﻿using LoboVaz.Services;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoboVaz.Controllers
+{
+
+    public class PostController : Controller
+    {
+
+        public IPostService PostService { get; set; }
+
+        public PostController(IPostService PostService)
+        {
+            this.PostService = PostService;
+        }
+
+
+        [HttpGet]
+        public JsonResult list(String filter,int page)
+        {
+
+            return Json(PostService.load(page,filter), JsonRequestBehavior.AllowGet);
+        }
+
+
+
+
+
+
+
+    }
+}
