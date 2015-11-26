85ec285134587966b8ec2596744b871c0147f755
dlermen12 <dennerl@hotmail.com>
2015-11-25 22:51:35 -0200

    Criação do Controller e confguração do Unity para injetar dependencias

---
+++ Domain/Models/MailerModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LoboVaz.Models
+{
+    public class MailerModel
+    {
+        public bool Sale { get; set; }
+
+        public string Name { get; set; }
+
+        public string GrassType { get; set; }
+
+        public string Phone { get; set; }
+
+        public string FromEmail { get; set; }
+
+        public string Message { get; set; }
+
+        public string ToEmail { get; set; }
+        public string Quantity { get; set; }
+        
+    }
+}
