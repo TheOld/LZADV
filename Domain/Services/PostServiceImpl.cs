e1319d5ebd79b7de3cd647705d9c0f0161d61ee4
dlermen12 <dennerl@hotmail.com>
2015-11-25 22:37:04 -0200

    Criação do Controller e confguração do Unity para injetar dependencias

---
+++ Domain/Services/PostServiceImpl.cs
@@ -0,0 +1,24 @@
+﻿using LoboVaz.Domain.DAO;
+using LoboVaz.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LoboVaz.Services
+{
+    public class PostServiceImpl : IPostService
+    {
+        public IPostDAO PostDAO { get; set; }
+
+        public PostServiceImpl(IPostDAO PostDAO) {
+            this.PostDAO = PostDAO;
+        }
+
+        public List<Post> load(int page,string filter)
+        {
+            return PostDAO.load(page,filter);
+        }
+
+    }
+}
